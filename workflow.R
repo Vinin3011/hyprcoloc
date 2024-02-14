081eb8d2feeda8713ba28c6c0eb059502e378fce
Jesus Nieto Reyes <jesusnietoreyes@yahoo.com>
2024-02-14 15:11:31 +0100

    merged MS and ALS data frames for testing and removed NA values

--- workflow.R
+++ workflow.R
@@ -1,7 +1,6 @@
 library(hyprcoloc)
 library(dplyr)
 library(data.table)
-hyprcoloc(betas, ses, trait.names=traits, snp.id=rsid)
 
 #data1 <- read.table(gzfile("Harmonized GWAS results/single_GWAS/pmid21833088_MS_eur.tsv.gz"), header = TRUE, sep = "\t")
 #data2 <- read.table(gzfile("Harmonized GWAS results/single_GWAS/pmid24076602_MS_eur.tsv.gz"), header = TRUE, sep = "\t")
@@ -21,20 +20,27 @@ t2d_paths <-c()
 pd_paths <-c()
 
 
-
+# MS
 # Read each .gz file and bind the resulting data frames together
 ms_betas <- lapply(ms_paths, function(ms_paths) {
   # Read the data and explicitly specify the column types to ensure consistency
-  data <- read.table(gzfile(ms_paths), header = TRUE, sep = "\t",na.strings = "-NA", stringsAsFactors = FALSE, colClasses = c(beta_effect = "numeric")) 
+  data <- read.table(gzfile(ms_paths), header = TRUE, sep = "\t",na.strings = "-NA", stringsAsFactors = FALSE, colClasses = c(beta_effect = "numeric"))
   new_column_name <- as.character(data$trait[1])
   data %>%
     select(SNP, beta_effect) %>%
     rename(!!new_column_name := beta_effect)
-}) %>% bind_rows()
+}) %>% bind_rows() # combine into single data frame
 
+# remove NA values
+ms_betas <- ms_betas[ms_betas$SNP != "NA", ]
+
+# remove duplicates
+ms_betas_unique <- ms_betas[!duplicated(ms_betas$SNP), ]
+
+# Check if values in SNP are unique and print the non-unique values if present
 
 # Get counts of each SNP value
-snp_counts <- table(ms_betas$SNP)
+snp_counts <- table(ms_betas_unique$SNP)
 
 # Filter for non-unique values
 non_unique_values <- names(snp_counts)[snp_counts > 1]
@@ -45,3 +51,40 @@ if (length(non_unique_values) == 0) {
   print("Non-unique values in the 'SNP' column along with their counts:")
   print(data.frame(SNP = non_unique_values, Count = snp_counts[non_unique_values]))
 }
+
+# ALS
+# Read each .gz file and bind the resulting data frames together
+als_betas <- lapply(als_paths, function(als_paths) {
+  # Read the data and explicitly specify the column types to ensure consistency
+  data <- read.table(gzfile(als_paths), header = TRUE, sep = "\t",na.strings = "-NA", stringsAsFactors = FALSE, colClasses = c(beta_effect = "numeric"))
+  new_column_name <- as.character(data$trait[1])
+  data %>%
+    select(SNP, beta_effect) %>%
+    rename(!!new_column_name := beta_effect)
+}) %>% bind_rows() # combine into single data frame
+
+# remove NA values
+als_betas <- als_betas[als_betas$SNP != "NA", ]
+
+# remove duplicates
+als_betas_unique <- als_betas[!duplicated(als_betas$SNP), ]
+
+# Check if values in SNP are unique and print the non-unique values if present
+
+# Get counts of each SNP value
+snp_counts <- table(als_betas_unique$SNP)
+
+# Filter for non-unique values
+non_unique_values <- names(snp_counts)[snp_counts > 1]
+
+if (length(non_unique_values) == 0) {
+  print("All values in the 'SNP' column are unique.")
+} else {
+  print("Non-unique values in the 'SNP' column along with their counts:")
+  print(data.frame(SNP = non_unique_values, Count = snp_counts[non_unique_values]))
+}
+
+# Merge the data frames based on the "SNP" column
+merged_betas <- merge(ms_betas_unique, als_betas_unique, by = "SNP")
+
+
