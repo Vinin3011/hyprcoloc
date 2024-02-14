be6b0d8c365b58ba6be6ac3e48d7a4464fadeb9c
Jesus Nieto Reyes <jesusnietoreyes@yahoo.com>
2024-02-14 14:35:15 +0100

    Specified more paths and remove duplicates in merged data frames

--- workflow.R
+++ workflow.R
@@ -1,12 +1,26 @@
 library(hyprcoloc)
 library(dplyr)
+library(data.table)
 hyprcoloc(betas, ses, trait.names=traits, snp.id=rsid)
 
 #data1 <- read.table(gzfile("Harmonized GWAS results/single_GWAS/pmid21833088_MS_eur.tsv.gz"), header = TRUE, sep = "\t")
 #data2 <- read.table(gzfile("Harmonized GWAS results/single_GWAS/pmid24076602_MS_eur.tsv.gz"), header = TRUE, sep = "\t")
 #data3 <- read.table(gzfile("Harmonized GWAS results/single_GWAS/pmid27386562_MS_eur.tsv.gz"), header = TRUE, sep = "\t")
 
-ms_paths <- c("Harmonized GWAS results/single_GWAS/pmid21833088_MS_eur.tsv.gz", "Harmonized GWAS results/single_GWAS/pmid24076602_MS_eur.tsv.gz", "Harmonized GWAS results/single_GWAS/pmid27386562_MS_eur.tsv.gz")
+ms_paths <- c("Harmonized GWAS results/single_GWAS/pmid21833088_MS_eur.tsv.gz", 
+              "Harmonized GWAS results/single_GWAS/pmid24076602_MS_eur.tsv.gz", 
+              "Harmonized GWAS results/single_GWAS/pmid27386562_MS_eur.tsv.gz")
+
+als_paths <-c("Harmonized GWAS results/single_GWAS/pmid27455348_ALS_eur.tsv.gz", 
+              "Harmonized GWAS results/single_GWAS/pmid28931804_ALS_eur-eas.tsv.gz",
+              "Harmonized GWAS results/single_GWAS/pmid29566793_ALS.tsv.gz",
+              "Harmonized GWAS results/single_GWAS/pmid29566793_ALS_eur.tsv.gz",
+              "Harmonized GWAS results/single_GWAS/pmid34873335_ALS_eur.tsv.gz",
+              "Harmonized GWAS results/single_GWAS/pmid34873335_ALS_eur-eas.tsv.gz")
+t2d_paths <-c()
+pd_paths <-c()
+
+
 
 # Read each .gz file and bind the resulting data frames together
 ms_betas <- lapply(ms_paths, function(ms_paths) {
@@ -19,7 +33,15 @@ ms_betas <- lapply(ms_paths, function(ms_paths) {
 }) %>% bind_rows()
 
 
+# Get counts of each SNP value
+snp_counts <- table(ms_betas$SNP)
 
+# Filter for non-unique values
+non_unique_values <- names(snp_counts)[snp_counts > 1]
 
-
-
+if (length(non_unique_values) == 0) {
+  print("All values in the 'SNP' column are unique.")
+} else {
+  print("Non-unique values in the 'SNP' column along with their counts:")
+  print(data.frame(SNP = non_unique_values, Count = snp_counts[non_unique_values]))
+}
