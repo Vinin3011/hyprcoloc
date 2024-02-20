1c991911fdb1daee8cd215eb9910ac4c4c4cd7fb
Vinh Tran <vinh.t3011@gmail.com>
2024-02-20 16:37:17 +0100

    Changes some Subgropus of Traits

--- prepocessing.R
+++ prepocessing.R
@@ -10,18 +10,18 @@ metabolites <- c("Ser", "IL1", "PRTN3")
 # Diseases
 
 autoimmune_diseases <- c("MS", "RA", "T2D")
-inflammatory_diseases <- c("PD", "ALS", "DLB", "SCZ")
+inflammatory_diseases <- c("PD", "ALS", "DLB", "SCZ", "RA")
 
 # Cytokines
 
-inflammatory_cytokines <- c("IL1b", "IL6", "TNFa", "IL17", "IL8", "MCP1", "RANTES", "MIF", "IL12p70", "IL18", "IFNg", "MIG", "ip10")
-anti_inflammatory_cytokines <- c("IL10", "IL1ra", "IL4", "IL13", "IL9")
-growth_factors <- c("HGF", "FGF", "GCSF", "MCSF", "SCF", "PDGFbb")
-chemokines <- c("Eotaxin", "CTACK", "MIP1a", "MIP1b", "SDF1", "SCGFb", "TRAIL")
+pro_inflammatory <- c("IL1b", "IL6", "TNFa", "IL17", "IFNg", "IL12p70", "IL18", "IL23")
+anti_inflammatory <- c("IL10", "IL1ra", "IL4", "IL13", "IL9")
+chemokines <- c("MCP1", "MIP1a", "MIP1b", "RANTES", "IP10", "eotaxin", "MIG", "SDF1", "CTACK", "SCF", "SCGFb")
+growth_factors <- c("FGF", "GCSF", "HGF", "PDGFbb", "MCSF", "TRAIL", "bNGF", "GROa")
+others <- c("CRP", "C3", "APH", "IL2", "IL2ra", "IL5", "IL7", "IL8", "IL16", "IL2", "IL3", "IL15")
 
 
-
-traits_of_interest <- list("Ser", "IL1", "PRTN3")
+traits_of_interest <- list("CTACK", "IL18", "MIP1b", "IL8", "T2D")
 
 
 # paths for cytokines and single GWAS
